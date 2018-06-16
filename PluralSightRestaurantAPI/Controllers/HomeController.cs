1d4106920d7c47a1f126941a82d0e0842b4f8bca
eberki <gonosztaviszorny@gmail.com>
2018-06-16 17:01:22 +0200

    Add project files.

---
+++ PluralSightRestaurantAPI/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluralSightRestaurantAPI.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello From the HomeContoller";
+        }
+    }
+}
---
+++ PluralSightRestaurantAPI/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace PluralSightRestaurantAPI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseWelcomePage(new WelcomePageOptions() { Path="/wp" });
+
+            app.Use(next =>
+            {
+                return async context =>
+                {
+                    logger.LogInformation("Request incoming.");
+                    if (context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Hit!");
+                        logger.LogInformation("Request handled.");
+                    }
+                    else
+                    {
+                        await next(context);
+                        logger.LogInformation("Request outgoing");
+                    }
+                };
+            });
+
+
+            //app.UseDefaultFiles();
+
+
+            app.UseStaticFiles();
+
+            //app.UseMvcWithDefaultRoute();
+
+            app.UseMvcWithDefaultRoute();
+
+
+            app.Run(async (context) =>
+            {
+
+                //string greetingText = configuration["greeting"];
+                string greetingText = greeter.GetMessageOfTheDay(); 
+                //await context.Response.WriteAsync(configuration.GetValue<string>("Greeting"));
+                //await context.Response.WriteAsync($"{greetingText} : {env.EnvironmentName}");
+                await 
+            });
+        }
+    }
+}
