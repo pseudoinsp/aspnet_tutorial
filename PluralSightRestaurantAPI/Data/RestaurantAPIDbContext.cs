4d679686cb697d94c7e53b0e163229250e3b5114
eberki <gonosztaviszorny@gmail.com>
2018-07-15 12:11:50 +0200

    After module 6 (EF)

---
+++ PluralSightRestaurantAPI/Data/RestaurantAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PluralSightRestaurantAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluralSightRestaurantAPI.Data
+{
+    public class RestaurantAPIDbContext : DbContext
+    {
+        public RestaurantAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ PluralSightRestaurantAPI/Services/SqlResturantData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PluralSightRestaurantAPI.Data;
+using PluralSightRestaurantAPI.Models;
+
+namespace PluralSightRestaurantAPI.Services
+{
+    public class SqlResturantData : IRestaurantData
+    {
+        private RestaurantAPIDbContext _context;
+
+        public SqlResturantData(RestaurantAPIDbContext context)
+        {
+            _context = context;
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant);
+
+            // generally this should be somewhere else 
+            _context.SaveChanges();
+
+            return restaurant; 
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
